04f1e4fbbff0f6f2cb38057297eaf23a306470ef
rassoulsall11-boop <rassoulsall11@gmail.com>
2026-02-09 07:57:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,30 @@ initEngine();
 initPlayer();
 initUI();
 requestAnimationFrame(gameLoop);
+import { initMoteur } from './moteur.js';
+import { initJoueur } from './joueur.js';
+import { loop } from './boucle.js';
+import { initUI } from './ui.js';
+
+const canvas = document.getElementById('game');
+
+const input = { z:false, q:false, s:false, d:false };
+
+window.addEventListener('keydown', e => {
+  if (e.key === 'z') input.z = true;
+  if (e.key === 's') input.s = true;
+  if (e.key === 'q') input.q = true;
+  if (e.key === 'd') input.d = true;
+});
+
+window.addEventListener('keyup', e => {
+  if (e.key === 'z') input.z = false;
+  if (e.key === 's') input.s = false;
+  if (e.key === 'q') input.q = false;
+  if (e.key === 'd') input.d = false;
+});
+
+initMoteur(canvas);
+initJoueur();
+initUI();
+loop(input);
