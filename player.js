6f318286038e1cde5862f5f717575175c6a5db4a
rassoulsall11-boop <rassoulsall11@gmail.com>
2026-02-09 07:53:43 +0100

    Update player.js

--- player.js
+++ player.js
@@ -1,14 +1,27 @@
-let playerMesh;
+import { ÉTAT } from './état.js';
+import { scene, camera } from './moteur.js';
 
-function initPlayer() {
-  playerMesh = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 32, 32),
-    new THREE.MeshStandardMaterial({ color: 0x00f2ff })
-  );
-  scene.add(playerMesh);
+export let joueurMesh;
+
+export function initJoueur() {
+  const geo = new THREE.SphereGeometry(0.5, 16, 16);
+  const mat = new THREE.MeshStandardMaterial({ color: 0x00ffff });
+
+  joueurMesh = new THREE.Mesh(geo, mat);
+  joueurMesh.position.y = 1;
+
+  scene.add(joueurMesh);
 }
 
-function updatePlayer() {
-  const p = GameState.player;
-  playerMesh.position.set(p.x, p.y, p.z);
+export function updateJoueur(input) {
+  const v = ÉTAT.joueur.vitesse;
+
+  if (input.z) joueurMesh.position.z -= v;
+  if (input.s) joueurMesh.position.z += v;
+  if (input.q) joueurMesh.position.x -= v;
+  if (input.d) joueurMesh.position.x += v;
+
+  camera.position.x = joueurMesh.position.x;
+  camera.position.z = joueurMesh.position.z + 8;
+  camera.lookAt(joueurMesh.position);
 }
